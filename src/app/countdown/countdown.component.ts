8fb8e012376cffbd8e1f6192c95de4075aac59f7
sabelogumede <iamsabelogumede@gmail.com>
2021-06-14 01:16:26 +0200

    created app with two child components named countdown and progress bar. Then created communication between the componets and App parent component using input, output and emit, counter template refference and lifecyle hooks.

--- src/app/countdown/countdown.component.ts
+++ src/app/countdown/countdown.component.ts
@@ -1,12 +1,76 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, OnInit, OnDestroy, OnChanges, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-countdown',
   templateUrl: './countdown.component.html',
   styleUrls: ['./countdown.component.scss']
 })
-export class CountdownComponent{
+export class CountdownComponent implements OnInit, OnDestroy, OnChanges{
+  // ***Lifesycle hooks**
+  ngOnInit(): void {
+    this.startCountdown();
+  }
+
+  ngOnDestroy():void{
+    this.clearTimeout();
+  }
+
+  ngOnChanges(changes):void{
+    console.log("init value updated to: ", changes.init.currentValue);
+    this.startCountdown();
+  }
+
+  // ****properties***
+  @Output() onDecrease = new EventEmitter<number>();
+  @Output() onComplete = new EventEmitter<void>();
+
+  @Input() init:number = null;
+  public counter:number = 0;
+  // Timer Private Ref
+  private countdownTimerRef:any = null;
 
   constructor() { }
+
+  // ***methods***
+
+  startCountdown(){
+    if(this.init && this.init >0){
+      // clear timer that is working
+      this.clearTimeout();
+      this.counter = this.init;
+      this.doCountdown();
+    }
+  }
+
+  doCountdown(){
+    // timer private ref
+    this.countdownTimerRef = setTimeout(()=>{
+      this.counter = this.counter -1;
+      this.processCountdown();
+    }, 1000);
+  }
+
+  // clean timer ref - Method
+  private clearTimeout(){
+    if(this.countdownTimerRef){
+      clearTimeout(this.countdownTimerRef);
+      this.countdownTimerRef = null;
+    }
+  }
+
+  processCountdown(){
+    //emit event COUNT
+    this.onDecrease.emit(this.counter);
+    console.log("count is ", this.counter);
+
+    if(this.counter == 0){
+      //emit event COUNTER END
+      this.onComplete.emit();
+      console.log("--counter end--")
+    }
+    else{
+      this.doCountdown();
+    }
+  }
 
 }
